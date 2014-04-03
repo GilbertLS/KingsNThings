8a661324b2c532b3676df28b3519b0898b8c868d
Andrew Belu <andrewbelu@hotmail.com>
2014-04-03 19:57:47 -0400

    Fixed game not starting problem (if this was the issue, now that I look at it, it probably isn't)

--- KingsNThings/src/Game/Networking/GameServer.java
+++ KingsNThings/src/Game/Networking/GameServer.java
@@ -8,15 +8,18 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Semaphore;
 
 import Game.GameController;
 
 public class GameServer implements Runnable {
 	InetAddress address;
 	public static ArrayList<GameRouter> servers = new ArrayList<GameRouter>();
+	private Semaphore gameSetup;
 	
-	public GameServer() throws UnknownHostException {
+	public GameServer(Semaphore setupSem) throws UnknownHostException {
 		this.address = InetAddress.getLocalHost();
+		gameSetup = setupSem;
 	}
 	
 	public InetAddress GetAddress(){
@@ -26,6 +29,7 @@ public class GameServer implements Runnable {
 	public void startServer() {
 		try {
 			ServerSocket serverSocket = new ServerSocket(Protocol.GAMEPORT);
+			gameSetup.release();
 			while (!checkStartGame()) {
 		       Socket clientSocket = serverSocket.accept();
 		       System.out.println("A client has joined the game");
